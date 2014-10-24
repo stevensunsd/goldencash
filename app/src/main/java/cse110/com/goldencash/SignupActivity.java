428232caafdc59e856a507130d8dedc952e3a3e4
Xin Wen <wenxin3262@gmail.com>
2014-10-24 10:39:08 -0700

    added transition activity

--- app/src/main/java/cse110/com/goldencash/SignupActivity.java
+++ app/src/main/java/cse110/com/goldencash/SignupActivity.java
@@ -4,12 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by jeremywen on 10/21/14.
  */
-public class SignupActivity extends Activity {
+public class SignupActivity extends Activity implements View.OnClickListener {
 
+    Button cancelButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +24,22 @@ public class SignupActivity extends Activity {
         // Enable the "Up" button for more navigation options
         getActionBar().setDisplayHomeAsUpEnabled(true);
 
+        cancelButton = (Button) findViewById(R.id.button_cancel);
+        cancelButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(findViewById(R.id.button_cancel).equals(view)) {
+            finish();
+        }else{
+            signup();
+        }
+    }
+
+    private void signup() {
+        //create Parse user object
     }
 }
+
+//TODO: check all text field correctness, before "Parsing"
