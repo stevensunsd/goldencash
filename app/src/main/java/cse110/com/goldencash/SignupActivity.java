f87c8d3636eb92033e8e168b7a574f4050b3c4bd
xiaoping weng <xiao5612327@gmail.com>
2014-10-24 13:44:26 -0700

    added checkusername and checkpassword

--- app/src/main/java/cse110/com/goldencash/SignupActivity.java
+++ app/src/main/java/cse110/com/goldencash/SignupActivity.java
@@ -9,16 +9,29 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.parse.GetCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
 /**
  * Created by jeremywen on 10/21/14.
  */
 public class SignupActivity extends Activity implements View.OnClickListener {
 
     Button cancelButton;
+
+    boolean usernameOk;
+    String username;
+    EditText password1;
+    EditText password2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Parse.initialize(this, "1XwyXTQlIQDuwcjETTTmvEaysvJVZLsSasuxibY3",
+                "hQIEN0MfhYKiBPCHsPZ6djei7myOjcRpX56Cd4Xc");
         // Tell the activity which XML layout is right
         setContentView(R.layout.activity_signup);
         // Get the message from the intent
@@ -26,6 +39,14 @@ public class SignupActivity extends Activity implements View.OnClickListener {
 
         cancelButton = (Button) findViewById(R.id.button_cancel);
         cancelButton.setOnClickListener(this);
+
+        username = ((EditText)
+                findViewById(R.id.signup_username)).getText().toString().toLowerCase();
+
+        password1 = (EditText) findViewById(R.id.signup_password);
+
+        password2 = (EditText) findViewById(R.id.signup_password2);
+
     }
 
     @Override
@@ -39,6 +60,38 @@ public class SignupActivity extends Activity implements View.OnClickListener {
 
     private void signup() {
         //create Parse user object
+
+    }
+
+    private boolean CheckPassword(){
+
+        //check two passwords if equals
+        return (password1.getText().equals(password2.getText()));
+    }
+
+    private void CheckUsername(){
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("User");
+        query.whereEqualTo("username",username);
+        query.getFirstInBackground(new GetCallback<ParseObject>() {
+            public void done(ParseObject object, ParseException e) {
+
+
+                if (e == null) {
+                    // object will be your User
+                    Log.d(getString(R.string.debugInfo_text),"Found a exist username");
+                    // print out error massage username exist
+
+                    usernameOk = false;
+                } else {
+                    //username is ok to create
+                    Log.d(getString(R.string.debugInfo_text),"no username used OK to create");
+
+                    //set username to true
+                    usernameOk = true;
+                }
+            }
+        });
     }
 
     private void clearAlltext() {
